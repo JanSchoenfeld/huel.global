7d031299a8bb8d31dac08caf0686adb9cd8448c6
JanSchoenfeld <Jan.Schoenfeld@fh-muenster.de>
2019-03-03 19:36:26 +0100

    Commit to backup, minor design changes

--- src/public/js/sign-up.js
+++ src/public/js/sign-up.js
@@ -1,6 +1,14 @@
 function validate() {
     if (document.getElementById('password').value != document.getElementById('password-confirmation').value) {
-        alert(`Passwords don't match!`);
+        
+        document.getElementById("dp-alert").style.setProperty("display", "inline");
+        // console.log($("#dp-alert"));
+        // $("#dp-alert").fadeIn(() => {
+        
+        //     return false;
+        // });
+
+        // alert('Passwords do not match!');
         return false;
     } else {
         return true;
