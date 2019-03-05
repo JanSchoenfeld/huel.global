dbe5736887d6cbecb3d2654029449f96e4183f4c
JanSchoenfeld <Jan.Schoenfeld@fh-muenster.de>
2019-03-05 11:31:20 +0100

    Add Theme selection to navbar! TODO: persist themechanges throughout site change

--- src/public/js/particles.js
+++ src/public/js/particles.js
@@ -1,4 +1,6 @@
-whiteOnBlack = {
+const particlesCanvas = document.getElementById('particles-js');
+
+darkVector = {
     "particles": {
         "number": {
             "value": 110,
@@ -54,7 +56,7 @@ whiteOnBlack = {
         },
         "move": {
             "enable": true,
-            "speed": 6,
+            "speed": 3,
             "direction": "none",
             "random": false,
             "straight": false,
@@ -220,6 +222,134 @@ defaultParticles = {
     "retina_detect": true
 };
 
+stars = {
+    "particles": {
+        "number": {
+            "value": 160,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 1,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 1,
+                "opacity_min": 0,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 4,
+                "size_min": 0.3,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": false,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 1,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 600
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "bubble"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 250,
+                "size": 0,
+                "duration": 2,
+                "opacity": 0,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 400,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+};
+
+function defaultTheme() {
+    particlesCanvas.style.setProperty("background-color", "#cc0b0b");
+    return particlesJS("particles-js", defaultParticles);
+}
+
+function darkTheme() {
+    particlesCanvas.style.setProperty("background-color", "#000000");
+    return particlesJS("particles-js", darkVector);
+}
+
+function starTheme() {
+    particlesCanvas.style.setProperty("background-color", "#262b4d");
+    return particlesJS("particles-js", stars);
+}
 
 
-particlesJS("particles-js", whiteOnBlack);
+
+
+
+particlesJS("particles-js", darkVector);
